9add0b82e4b93a654f7b944ef5cf0fe26779d9f5
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2021-07-30 15:21:04 +0400

    Продолжаю работать над первой страницей

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -85,5 +85,48 @@
     breakpointDesktop.addListener(breakpointChecker);
     breakpointChecker();
   };
+  const getTooltip = () => {
+    let tooltipElem;
+
+    document.onmouseover = function(event) {
+      // const tooltipButton = document.qs
+      const target = event.target;
+
+      // если у нас есть подсказка...
+      // let tooltipHtml = target.dataset.tooltip;
+      const tooltipHtml = target.closest('.tooltip__button');
+      if (!tooltipHtml) return;
+      console.log(tooltipHtml);
+
+      // ...создадим элемент для подсказки
+
+      tooltipElem = document.createElement('div');
+      tooltipElem.className = 'tooltip';
+      tooltipElem.innerHTML = tooltipHtml;
+      document.body.append(tooltipElem);
+
+      // спозиционируем его сверху от аннотируемого элемента (top-center)
+      let coords = target.getBoundingClientRect();
+
+      let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+      if (left < 0) left = 0; // не заезжать за левый край окна
+
+      let top = coords.top - tooltipElem.offsetHeight - 5;
+      if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+        top = coords.top + target.offsetHeight + 5;
+      }
+
+      tooltipElem.style.left = left + 'px';
+      tooltipElem.style.top = top + 'px';
+    };
+
+    document.onmouseout = function(e) {
+      if (tooltipElem) {
+        tooltipElem.remove();
+        tooltipElem = null;
+      }
+    };
+  };
   getSlider();
+  getTooltip();
 })();
