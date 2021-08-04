2bdb5f4e284ba16be008090ea43a986cd7a59a6e
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2021-08-04 17:17:39 +0400

    Закончил основную верстку страницу, начал верстку попапов

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,6 +1,142 @@
 /* eslint-disable no-undef */
 (function () {
   'use strict';
+  const body = document.querySelector('body');
+  let unlock = true;
+  function bodyLock(delay) {
+    const body = document.querySelector('body');
+    if (body.classList.contains('_lock')) {
+      bodyLockRemove(delay);
+    } else {
+      bodyLockAdd(delay);
+    }
+  };
+  function bodyLockRemove(delay) {
+    const body = document.querySelector('body');
+    if (unlock) {
+      const lockPadding = document.querySelectorAll('._lp');
+      setTimeout(() => {
+        for (let index = 0; index < lockPadding.length; index++) {
+          const el = lockPadding[index];
+          el.style.paddingRight = '0px';
+        }
+        body.style.paddingRight = '0px';
+        body.classList.remove('_lock');
+      }, delay);
+
+      unlock = false;
+      setTimeout(function () {
+        unlock = true;
+      }, delay);
+    }
+  };
+  function bodyLockAdd(delay) {
+    const body = document.querySelector('body');
+    if (unlock) {
+      const lockPadding = document.querySelectorAll('._lp');
+      for (let index = 0; index < lockPadding.length; index++) {
+        const el = lockPadding[index];
+        el.style.paddingRight =
+          window.innerWidth -
+          document.querySelector('.wrapper').offsetWidth +
+          'px';
+      }
+      body.style.paddingRight =
+        window.innerWidth -
+        document.querySelector('.wrapper').offsetWidth +
+        'px';
+      body.classList.add('_lock');
+
+      unlock = false;
+      setTimeout(function () {
+        unlock = true;
+      }, delay);
+    }
+  };
+  const getPopup = () => {
+    const popupLink = document.querySelectorAll('._popup-link');
+    const popups = document.querySelectorAll('.popup');
+    for (let index = 0; index < popupLink.length; index++) {
+      const el = popupLink[index];
+      el.addEventListener('click', function (e) {
+        if (unlock) {
+          const item = el.getAttribute('href').replace('#', '');
+          const video = el.getAttribute('data-video');
+          popupOpen(item, video);
+        }
+        e.preventDefault();
+      });
+    }
+    for (let index = 0; index < popups.length; index++) {
+      const popup = popups[index];
+      popup.addEventListener('click', function (e) {
+        if (!e.target.closest('.popup__body')) {
+          popupClose(e.target.closest('.popup'));
+        }
+      });
+    }
+    function popupOpen(item, video = '') {
+      const activePopup = document.querySelectorAll('.popup._active');
+      if (activePopup.length > 0) {
+        popupClose('', false);
+      }
+      const curentPopup = document.querySelector('.popup_' + item);
+      if (curentPopup && unlock) {
+        if (video !== '' && video != null) {
+          const popupVideo = document.querySelector('.popup_video');
+          popupVideo.querySelector('.popup__video').innerHTML =
+            '<iframe src="https://www.youtube.com/embed/' +
+            video +
+            '?autoplay=1"  allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+        }
+        if (!document.querySelector('.menu__body._active')) {
+          bodyLockAdd(500);
+        }
+        curentPopup.classList.add('_active');
+        history.pushState('', '', '#' + item);
+      }
+    }
+    function popupClose(item, bodyUnlock = true) {
+      if (unlock) {
+        if (!item) {
+          for (let index = 0; index < popups.length; index++) {
+            const popup = popups[index];
+            const video = popup.querySelector('.popup__video');
+            if (video) {
+              video.innerHTML = '';
+            }
+            popup.classList.remove('_active');
+          }
+        } else {
+          const video = item.querySelector('.popup__video');
+          if (video) {
+            video.innerHTML = '';
+          }
+          item.classList.remove('_active');
+        }
+        if (!document.querySelector('.menu__body._active') && bodyUnlock) {
+          bodyLockRemove(500);
+        }
+        history.pushState('', '', window.location.href.split('#')[0]);
+      }
+    }
+    const popupCloseIcon = document.querySelectorAll(
+      '.popup__close,._popup-close'
+    );
+    if (popupCloseIcon) {
+      for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function () {
+          popupClose(el.closest('.popup'));
+        });
+      }
+    }
+    document.addEventListener('keydown', function (e) {
+      if (e.code === 'Escape') {
+        popupClose();
+      }
+    });
+  };
   const getSlider = () => {
     const breakpointDesktop = window.matchMedia('(min-width: 1280px)');
     const breakpointTablet = window.matchMedia('(min-width: 768px)');
@@ -259,6 +395,7 @@
       });
     });
   };
+  getPopup();
   getSlider();
   getTooltip();
   getChangeRoofImages();
